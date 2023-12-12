43c9c75d8de6c4961bc7aa8f57c7b078176a1ce4
Bocah5Racun <onlymoderatelymad@gmail.com>
2023-12-12 13:46:01 +0800

    Troubleshooting critical error

--- index.php
+++ index.php
@@ -2,31 +2,7 @@
 
 <main>
     <section id="hero">
-        <?php
         
-            // grabs and displays the 5 latest news items
-            $args = array(
-                'post_type' => 'post',
-                'posts_per_page' => '5',
-                'orderby' => 'date',
-                'order' => 'DESC',
-                'category_name' => 'news'
-            );
-
-            $the_query = WP_Query( $args );
-
-            if( $the_query->have_posts() ) :
-                // show posts
-                while( $the_query->have_posts() ) :
-                    $the_query->the_post();
-                    the_title();
-                    the_excerpt();
-                endwhile;
-            endif;
-            
-            wp_reset_postdata();
-
-        ?>
     </section>
     <section class="adspace--lg--landscape"></section>
     <section id="latest-news"></section>
