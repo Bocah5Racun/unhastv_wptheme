0578e431caf895b0e72e05365354e826797a634a
Bocah5Racun <onlymoderatelymad@gmail.com>
2024-01-10 16:08:57 +0800

    Added ad-controls.js.

--- assets/scripts/ad-controls.js
+++ assets/scripts/ad-controls.js
@@ -1,15 +1,43 @@
 window.onload = () => {
+  // video gallery ad controls
   const videoGalleryBlock = document.querySelector(
     ".video-gallery__video .adsbygoogle"
   );
   const videoGalleryVideo = document.querySelector(
     ".video-gallery__video:nth-child(2)"
   );
-  const vGVWidth = videoGalleryVideo.clientWidth;
-  const vGVHeight = videoGalleryVideo.clientHeight;
+  const vGVWidth = videoGalleryVideo.offsetWidth;
+  const vGVHeight = videoGalleryVideo.offsetHeight;
 
-  console.log(videoGalleryBlock);
+  videoGalleryBlock.style.width = videoGalleryBlock.parentElement.width =
+    vGVWidth;
+  videoGalleryBlock.style.height = videoGalleryBlock.parentElement.height =
+    vGVHeight;
 
-  videoGalleryBlock.style.width = vGVWidth;
-  videoGalleryBlock.style.height = vGVHeight;
+  // two-cols-ft-left
+  const twoColesLeftAd = document.querySelector(
+    ".template--two-columns-feature-left__inner-container .section__news-item .adsbygoogle"
+  );
+  const twoColsCenterBlock = document.querySelector(
+    ".template--two-columns-feature-left__inner-container .section__news-item:nth-child(2)"
+  );
+  const twoColsCenterWidth = twoColsCenterBlock.offsetWidth;
+  const twoColsCenterHeight = twoColsCenterBlock.offsetHeight;
+
+  twoColesLeftAd.style.width = twoColsCenterWidth;
+  twoColesLeftAd.style.height = twoColsCenterHeight;
+
+  // three-cols-ft-center ad controls
+  const threeColsCenterAd = document.querySelector(
+    ".template--three-columns-feature-center__inner-container .section__news-item .adsbygoogle"
+  );
+  console.log(threeColsCenterAd);
+  const threeColsCenterBlock = document.querySelector(
+    ".template--three-columns-feature-center__inner-container .section__news-item:nth-child(2)"
+  );
+  const threeColsCenterWidth = threeColsCenterBlock.offsetWidth;
+  const threeColsCenterHeight = threeColsCenterBlock.offsetHeight;
+
+  threeColsCenterAd.style.width = threeColsCenterWidth;
+  threeColsCenterAd.style.height = threeColsCenterHeight;
 };
