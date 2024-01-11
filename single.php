622c0d4c675968085c5b2067b92e123966f577a9
Bocah5Racun <onlymoderatelymad@gmail.com>
2024-01-11 12:16:03 +0800

    Tweaking single.php ad.

--- single.php
+++ single.php
@@ -19,17 +19,19 @@ add_filter( 'the_content', 'prefix_insert_post_ads' );
 function prefix_insert_post_ads( $content ) {
 	//CHANGE BELOW AdSense CODE WITH YOUR OWN CODE
 	$ad_code = '
-    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3215141506790563"
-     crossorigin="anonymous"></script>
-<ins class="adsbygoogle"
-     style="display:block; text-align:center;"
-     data-ad-layout="in-article"
-     data-ad-format="fluid"
-     data-ad-client="ca-pub-3215141506790563"
-     data-ad-slot="8727660920"></ins>
-<script>
-     (adsbygoogle = window.adsbygoogle || []).push({});
-</script>
+    <div class="single-adspace">
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3215141506790563"
+            crossorigin="anonymous"></script>
+        <ins class="adsbygoogle"
+            style="display:block; text-align:center;"
+            data-ad-layout="in-article"
+            data-ad-format="fluid"
+            data-ad-client="ca-pub-3215141506790563"
+            data-ad-slot="8727660920"></ins>
+        <script>
+            (adsbygoogle = window.adsbygoogle || []).push({});
+        </script>
+    </div>
 ';
     if ( is_single() ) {
 	//CHANGE 3 TO DESIRED NUMBER YOU WANT ADVERT TO BE APPEARED
